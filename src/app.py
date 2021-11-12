4029191c0185ed74953d172308786b9d59ec58b1
arykau <74668998+arykau@users.noreply.github.com>
2021-11-12 09:05:35 +0600

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 
 @app.route('/coin',methods=['POST','GET'])
 def coin():
-    
     if request.method == 'POST':
         coin_name = request.form['name'].lower()
-        print(coin_name)
         URL = "https://coinmarketcap.com/currencies/" + coin_name
         page = requests.get(URL)
         scrapper = BeautifulSoup(page.content,"lxml")
-        infos = scrapper.find("div", class_ = "sc-16r8icm-0 kjciSH contentClosed hasShadow").find_all("p")
+        infos = scrapper.find("div", class_ = "sc-16r8icm-0 kjciSH contentClosed hasShadow")
+        print(infos)
         for info in infos:
             data = info.text
             return render_template('index.html', data=data)
